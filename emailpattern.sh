c9775e059034b4b391ab310fff79fa7455f6c3bb
Biru4015 <rambirendra12@gmail.com>
2020-03-26 20:49:52 +0530

    [Birendra] Adding second part in last position in email pattern

--- emailpattern.sh
+++ emailpattern.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 echo "Enter any email"
 read email
-emailid="^([a-z]+)(\.[a-z0-9_\+\-]+)?@([a-z]+)\.([a-z]{2,4})$"
+emailid="^([a-z]+)(\.[a-z0-9_\+\-]+)?@([a-z]+)\.([a-z]{2,4})(\.[a-z]{2})?$"
 if [[ $email =~ $emailid ]]
 then
  echo "VALID EMAIL"
