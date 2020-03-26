56cb78e30571f751c8f026f740c5fafaf14e2d03
Biru4015 <rambirendra12@gmail.com>
2020-03-26 20:23:01 +0530

    [Birendra] Adding second valid mandatory part in email pattren

--- emailpattern.sh
+++ emailpattern.sh
@@ -2,10 +2,33 @@
 shopt -s extglob
 echo "Enter any email"
 read email
-emailid="^([a-z]+)$"
+emailid="^([a-z]+)@([a-z]+)$"
 if [[ $email =~ $emailid ]]
 then
  echo "VALID EMAIL"
 else
    echo "INVALID EMAIL"
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
